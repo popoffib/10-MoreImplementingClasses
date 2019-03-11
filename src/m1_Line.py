8911dbde5e2078d8c474829904aa41a247be4df5
popoffib <popoffib@rose-hulman.edu>
2019-03-11 00:13:29 -0400

    done with todo11

--- src/m1_Line.py
+++ src/m1_Line.py
@@ -598,7 +598,7 @@ class Line(object):
           :rtype: Point
         """
         # ---------------------------------------------------------------------
-        # TODO: 11.
+        # DONE: 11.
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
@@ -606,6 +606,12 @@ class Line(object):
         #        The tests are already written (below).
         #        They include the Example in the above doc-string.
         # ---------------------------------------------------------------------
+        dx = (self.start.x - self.end.x) / 2
+        dy = (self.start.y - self.end.y) / 2
+        mx = self.start.x - dx
+        my = self.start.y - dy
+        midpoint = Point(mx, my)
+        return midpoint
 
     def is_parallel(self, line2):
         """
